99de77ba79e9cc7f705383a47998e0db44205b84
Matthew McKee <matthewmckee04@yahoo.co.uk>
2024-02-26 17:53:34 +0000

    Add Models and some templates.

--- app/apps.py
+++ app/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class CalendarAppConfig(AppConfig):
-    name = 'calendar_app'
+    name = "calendar_app"
---
+++ app/models.py
@@ -0,0 +1,55 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    date_of_birth = models.DateField(null=True)
+    profile_picture_url = models.CharField(max_length=128)
+
+
+class Calendar(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    events = models.ManyToManyField("Event", related_name="calendars")
+    name = models.CharField(max_length=64)
+    description = models.CharField(max_length=512)
+    colour = models.CharField(max_length=64)
+
+
+class Reminder(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    event = models.ForeignKey("Event", on_delete=models.CASCADE)
+    reminder_date_time = models.DateTimeField()
+    message = models.CharField(max_length=256)
+
+
+class Tag(models.Model):
+    events = models.ManyToManyField("Event", related_name="tags")
+    name = models.CharField(max_length=64)
+    description = models.CharField(max_length=512)
+    colour = models.CharField(max_length=64)
+
+
+class Event(models.Model):
+    owner = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="owned_events"
+    )
+    attendees = models.ManyToManyField(User, related_name="attended_events")
+    description = models.CharField(max_length=512)
+    start_date_time = models.DateTimeField()
+    end_date_time = models.DateTimeField()
+    location_latitude = models.FloatField(null=True, blank=True)
+    location_longitude = models.FloatField(null=True, blank=True)
+    recurring = models.BooleanField(default=False)
+    recurring_frequency = models.CharField(max_length=64, blank=True)
+
+
+class Invitation(models.Model):
+    sender = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="sent_invitations"
+    )
+    receiver = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="received_invitations"
+    )
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    status = models.CharField(max_length=32)
+    message = models.CharField(max_length=256)
